5805ab8675a75c009984788949dfb85c11f3ee03
stephen lloyd <stephen.lloyd85@gmail.com>
2017-02-24 14:52:48 +0000

    Modified test to read from file instead of stdin This will enable us to automate a lot of the testing to be more consistent amoungst applicants.

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,27 +1,20 @@
 class BitmapEditor
 
-  def run
-    @running = true
-    puts 'type ? for help'
-    while @running
-      print '> '
-      input = gets.chomp
-      case input
-        when '?'
-          show_help
-        when 'X'
-          exit_console
-        else
+  def run(file)
+    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+    
+    File.open(file).each do |line|
+      line = line.chomp
+      case line
+      when 'S'
+          puts "output image"
+      else
           puts 'unrecognised command :('
       end
     end
   end
 
   private
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
 
     def show_help
       puts "? - Help
@@ -30,7 +23,6 @@ C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
 V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image
-X - Terminate the session"
+S - Show the contents of the current image"
     end
 end
