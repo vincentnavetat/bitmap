6e63a87b409f369899490fbebb9c2a2dabad2de2
stephen lloyd <stephen.lloyd85@gmail.com>
2017-02-24 14:56:22 +0000

    removed show_help commands as no longer needed

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -2,7 +2,7 @@ class BitmapEditor
 
   def run(file)
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
-    
+
     File.open(file).each do |line|
       line = line.chomp
       case line
@@ -13,16 +13,4 @@ class BitmapEditor
       end
     end
   end
-
-  private
-
-    def show_help
-      puts "? - Help
-I M N - Create a new M x N image with all pixels coloured white (O).
-C - Clears the table, setting all pixels to white (O).
-L X Y C - Colours the pixel (X,Y) with colour C.
-V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image"
-    end
 end
