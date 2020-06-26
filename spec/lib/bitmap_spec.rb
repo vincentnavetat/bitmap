ae4322f36f7ca301e4f9b5c8a99dd71d6db5f0e5
Vincent Navetat <vincent.navetat@carwow.co.uk>
2020-06-26 18:09:04 +0100

    Add basic tests for bitmap rendering

--- spec/lib/bitmap_spec.rb
+++ spec/lib/bitmap_spec.rb
@@ -3,11 +3,20 @@
 require 'bitmap'
 
 RSpec.describe Bitmap do
-  let(:bitmap) { Bitmap.new }
+  let(:empty_bitmap) { Bitmap.new }
+  let(:bitmap) { Bitmap.new(3, 2) }
 
-  context 'when no size given' do
-    it 'default size is 1x1' do
-      expect(bitmap.render).to eq('O')
+  describe '#render' do
+    context 'for a bitmap with no size given' do
+      it 'renders a white 1x1 bitmap' do
+        expect(empty_bitmap.render).to eq('O')
+      end
+    end
+
+    context 'for a white 3x2 size bitmap' do
+      it 'renders a white 3x2 bitmap' do
+        expect(bitmap.render).to eq("OOO\nOOO")
+      end
     end
   end
 end
