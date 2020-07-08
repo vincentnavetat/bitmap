ae154f150e172fb26de580aa236b16f51fe577cb
Vincent Navetat <vincent.navetat@carwow.co.uk>
2020-07-08 08:46:11 +0100

    WIP fill

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -31,6 +31,20 @@ class Bitmap
     end
   end
 
+
+  def fill(pos_x, pos_y, color)
+    matching_color = pixel_color(pos_x, pos_y)
+    pixels_to_fill = [[pos_x, pos_y]]
+
+    while pixels_to_fill.length > 0 do
+      fill_now = pixels_to_fill.shift
+
+      color_pixel(fill_now[0], fill_now[1], color)
+
+      pixels_to_fill = add_neighboor_pixels(pixels_to_fill, fill_now, matching_color)
+    end
+  end
+
   def render
     @pixels.join("\n")
   end
@@ -41,5 +55,25 @@ class Bitmap
 
   def segment_values(first, second)
     first < second ? (first..second) : (second..first)
+  end
+
+  def pixel_color(pos_x, pos_y)
+    @pixels[pos_x - 1][pos_y - 1]
+  end
+
+  def add_neighboor_pixels(pixel_array, fill_now, matching_color)
+    # up
+    pixel_array << [fill_now[0], fill_now[1] - 1] if pixel_color(fill_now[0], fill_now[1] - 1) == matching_color
+
+    # right
+    pixel_array << [fill_now[0] + 1, fill_now[1]] if pixel_color(fill_now[0] + 1, fill_now[1]) == matching_color
+
+    # bottom
+    pixel_array << [fill_now[0], fill_now[1] + 1] if pixel_color(fill_now[0], fill_now[1] + 1) == matching_color
+
+    # left
+    pixel_array << [fill_now[0] - 1, fill_now[1]] if pixel_color(fill_now[0] - 1, fill_now[1]) == matching_color
+
+    pixel_array
   end
 end
